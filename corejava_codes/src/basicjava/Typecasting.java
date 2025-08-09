6129b1aebc812b83f4d25f0de67a387aeba02aef
usha <ushai.22ds@saividya.ac.in>
2025-08-09 21:27:06 +0530

    second commit

---
+++ corejava_codes/src/basicjava/Typecasting.java
@@ -0,0 +1,22 @@
+package basicjava;
+
+public class Typecasting {
+	public static void main(String[] args) {
+		//widening 
+		int quatity=3;
+		double priceperitem=99.50;
+		double totalprice= quatity*priceperitem;
+		
+		//narrowing
+		
+		double discount=10.75;
+		int roundedDiscount =  (int)discount;
+		
+		double finalAmount =totalprice-roundedDiscount;
+		
+		System.out.println("Online Shopping summary");
+		System.out.println("final amount "+finalAmount);
+	}
+
+
+}
---
+++ corejava_codes/src/basicjava/Variable.java
@@ -0,0 +1,23 @@
+package basicjava;
+
+public class Variable {
+	int a=10;//instance variable
+	
+	void print()
+
+	{
+		String msg="hello";//local variable
+		System.out.println(msg);
+	}
+	
+	static String message="hello students";
+	
+	public static void main(String[] args) {
+		 Variable obj=new  Variable();
+		 System.out.println("the value of a " +obj.a);
+		 obj.print();
+		 System.out.println(message);
+	}
+	
+
+}
