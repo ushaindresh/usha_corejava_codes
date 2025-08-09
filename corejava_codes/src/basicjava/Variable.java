669a07534ff498aa5571204e9bc51bfd87488990
usha <ushai.22ds@saividya.ac.in>
2025-08-09 21:30:27 +0530

    third commit

--- corejava_codes/src/basicjava/Variable.java
+++ corejava_codes/src/basicjava/Variable.java
@@ -13,7 +13,8 @@ public class Variable {
 	static String message="hello students";
 	
 	public static void main(String[] args) {
-		 Variable obj=new  Variable();
+		 Variable obj=new  Variable
+				 ();
 		 System.out.println("the value of a " +obj.a);
 		 obj.print();
 		 System.out.println(message);
---
+++ corejava_codes/src/built_in_packages/Builtin_package_example.java
@@ -0,0 +1,17 @@
+package built_in_packages;
+
+public class Builtin_package_example {
+	public static void main(String[] args) {
+		String greeting="Hello world";
+		System.out.println("Original String -"+greeting);
+		System.out.println("Upper case -"+greeting.toUpperCase());
+		System.out.println("substring -"+greeting.substring(6));
+		
+		int max=Math.max(10,20);
+		double sqrt=Math.sqrt(25.0);
+		System.out.println("max number "+ max);
+		System.out.println("sqrt "+sqrt);
+		}
+
+
+}
